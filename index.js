2ca0d1fee3da3aa41149b338f47858c20c96a73f
Myroslav13 <myroslavshchuruk@gmail.com>
2025-12-12 22:02:38 +0200

    List view added

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const notes = ["Buy milk", "Finish homework"];
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {notes: notes});
+});
+
+app.post("/add", (req, res) => {
+  const newtItem = req.body.new_item;
+  
+  if (newtItem) {
+    notes.push(req.body.new_item);
+  }
+  
+  res.redirect("/");
 });
 
 app.listen(port, () => {

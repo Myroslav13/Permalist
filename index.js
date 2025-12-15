8e1bfed83bc767cf4edbdf9f30bf27befa2f9de5
Myroslav13 <myroslavshchuruk@gmail.com>
2025-12-15 22:52:22 +0200

    Added edit, delete, add request handling. Project completed

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 import express from 'express';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
-const notes = ["Buy milk", "Finish homework"];
+const notes = [];
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Udemy",
+  password: "myroslav13",
+  port: 5432,
+});
+
+db.connect();
+
+async function getAllData(db, notes) {
+  const result = await db.query("SELECT * FROM todotasks");
+  const data = result.rows;
+
+  data.forEach(element => {
+    notes.push(element);
+  });
+}
+
+app.get("/", async (req, res) => {
+  notes.length = 0;
+  await getAllData(db, notes);
   res.render("index.ejs", {notes: notes});
 });
 
-app.post("/add", (req, res) => {
-  const newtItem = req.body.new_item;
+app.post("/add", async (req, res) => {
+  const newItem = req.body.new_item;
   
-  if (newtItem) {
-    notes.push(req.body.new_item);
+  if (newItem) {
+    await db.query("INSERT INTO todotasks (task) VALUES ($1)", [newItem]);
+    res.redirect("/");
+  } else {
+    res.render("index.ejs", {notes: notes, error: "Enter something"});
   }
+});
+
+app.post("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedItem = req.body.edited_item;
   
+  if (updatedItem) {
+    await db.query("UPDATE todotasks SET task = $1 WHERE id = $2", [updatedItem, id]);
+    res.redirect("/");
+  } else {
+    res.render("index.ejs", {notes: notes, error: "Enter something"});
+  }
+});
+
+app.post("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await db.query("DELETE FROM todotasks WHERE id = $1", [id]);
   res.redirect("/");
 });
 
